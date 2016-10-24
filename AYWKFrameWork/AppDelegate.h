06c6a6d3011f283da9a13ecc9c3c967aba1df8ef
konglee <konglee873@gmail.com>
2016-10-24 09:42:56 +0800

    Initial Commit

---
+++ AYWKFrameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AYWKFrameWork
+//
+//  Created by kong on 16/10/24.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
